7fb925a7393fc3c2ea98dc08cc3996281361db5e
Mohitsharma44 <mohitsharma44@gmail.com>
2018-08-03 12:46:39 -0400

    fixed links

--- scripts/get_imdb_files.sh
+++ scripts/get_imdb_files.sh
@@ -5,13 +5,13 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo -e "Fetching Data from IMDB database \n"
 echo "Fetching Actors"
-curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/actors.list.gz
+curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/frozendata/actors.list.gz
 echo "Fetching Actresses"
-curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/actresses.list.gz
+curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/frozendata/actresses.list.gz
 echo "Fetching Genres"
-curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz
+curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/frozendata/genres.list.gz
 echo "Fetching Release Dates"
-curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/release-dates.list.gz
+curl -O -# ftp://ftp.fu-berlin.de/pub/misc/movies/database/frozendata/release-dates.list.gz
 
 echo -e "Building the csv file for UCSL ... "
 python ./build_imdb.py
